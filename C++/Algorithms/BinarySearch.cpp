b570be3768dfd78f59e87264b95ee53d85f326d6
Nikhil <55682350+Nikhil-prog@users.noreply.github.com>
2021-04-19 08:39:56 +0530

    Update BinarySearch.cpp

--- C++/Algorithms/BinarySearch.cpp
+++ C++/Algorithms/BinarySearch.cpp
@@ -1,1 +1,26 @@
+// function To search a number 'num' in array 'arr' within
+// range of [low, high] index
+// returns -1 in case of not exist
+int binarySearch(int *arr, int low, int high, int num)
+{
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        // it is same as (low+high)/2 but to avoid
+        // any overflow of int value while adding
 
+        if (arr[mid] == num)
+        {
+            return mid;
+        }
+        else if (arr[mid] < num)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
