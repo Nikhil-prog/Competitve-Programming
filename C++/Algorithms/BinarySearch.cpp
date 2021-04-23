cedd39baa6a2d78c933f4eade8187e618b65043d
Nikhil <55682350+Nikhil-prog@users.noreply.github.com>
2021-04-24 02:44:06 +0530

    Update BinarySearch.cpp

--- C++/Algorithms/BinarySearch.cpp
+++ C++/Algorithms/BinarySearch.cpp
@@ -1,14 +1,17 @@
+/*Nikhil*/
+
 // function To search a number 'num' in array 'arr' within
 // range of [low, high] index
 // returns -1 in case of not exist
-// Time Complexity - O(log (high-low+1))
+// TIME COMPLEXITY - O(log (n) )
+// where n: length of part of array on which search is applied
 int binarySearch(int *arr, int low, int high, int num)
 {
     while (low <= high)
     {
-        int mid = low + (high - low) / 2;
         // it is same as (low+high)/2 but to avoid
         // any overflow of int value while adding
+        int mid = low + (high - low) / 2;
 
         if (arr[mid] == num)
         {
