daaf64db0d6915e7da3870bf8b1ea6d7060661d3
Nikhil <55682350+Nikhil-prog@users.noreply.github.com>
2021-05-26 12:56:15 +0530

    Create SelectionSort.cpp

---
+++ C++/Sorting/SelectionSort.cpp
@@ -0,0 +1,37 @@
+/*Nikhil*/
+#include <iostream>
+using namespace std;
+
+void selectionSort(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        //swap part of selection sort
+        int tempVaule = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = tempVaule;
+    }
+}
+
+int main()
+{
+    int arr[] = {2, 9, -1, 5, 10, 1, 0, -7};
+    selectionSort(arr, 8);
+
+    //print sorted array
+    for (int i = 0; i < 8; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
