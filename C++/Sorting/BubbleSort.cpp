c48d26917e1cafdc2992a556d2cd40c2779cc1a4
Nikhil <55682350+Nikhil-prog@users.noreply.github.com>
2021-05-26 12:48:08 +0530

    Create BubbleSort.cpp

---
+++ C++/Sorting/BubbleSort.cpp
@@ -0,0 +1,33 @@
+/*Nikhil*/
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int *arr, int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int tempValue = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tempValue;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {2, 9, -1, 5, 10, 1, 0, -7};
+    bubbleSort(arr, 8);
+
+    //print sorted array
+    for (int i = 0; i < 8; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
