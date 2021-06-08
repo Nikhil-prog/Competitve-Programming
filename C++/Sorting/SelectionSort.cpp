d2682848fe814b354f2fc609d605b1c16b54bb0c
Nikhil <55682350+Nikhil-prog@users.noreply.github.com>
2021-06-08 11:20:57 +0530

    Update SelectionSort.cpp

--- C++/Sorting/SelectionSort.cpp
+++ C++/Sorting/SelectionSort.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void selectionSort(int *arr, int size)
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size - 1; i++)
     {
         int minIndex = i;
         for (int j = i + 1; j < size; j++)
