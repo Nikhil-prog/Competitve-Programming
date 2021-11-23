73cbe9d8441fcc863c8c1b4b894d1dc6ff78f1ea
Nikhil <55682350+Nikhil-prog@users.noreply.github.com>
2021-11-24 03:26:21 +0530

    Update balanced_parenthesis.cpp

--- C++/Stack & Queue/balanced_parenthesis.cpp
+++ C++/Stack & Queue/balanced_parenthesis.cpp
@@ -1,4 +1,10 @@
 /*Nikhil*/
+
+/*
+Given an expression string x. Examine whether the pairs and the orders of “{“,”}”,”(“,”)”,”[“,”]” are correct in exp.
+For example, the function should return 'true' for exp = “[()]{}{[()()]()}” and 'false' for exp = “[(])”.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
