1c9d07b58173baa9bfa4fbd09d8c01b0634f1964
Nikhil <55682350+Nikhil-prog@users.noreply.github.com>
2021-12-20 00:19:19 +0530

    Update middle_element_of_stack.cpp

--- C++/Stack & Queue/middle_element_of_stack.cpp
+++ C++/Stack & Queue/middle_element_of_stack.cpp
@@ -16,125 +16,124 @@ using namespace std;
 
 class myStack
 {
-	struct Node
-	{
-		int num;
-		Node *next;
-		Node *prev;
+    struct Node
+    {
+        int num;
+        Node *next;
+        Node *prev;
 
-		Node(int num)
-		{
-			this->num = num;
-		}
-	};
+        Node(int num)
+        {
+            this->num = num;
+        }
+    };
 
-	//Members of stack
-	Node *head = NULL;
-	Node *mid = NULL;
-	int size = 0;
+    // Members of stack
+    Node *head = NULL;
+    Node *mid = NULL;
+    int size = 0;
 
 public:
-	void push(int data)
-	{
-		Node *temp = new Node(data);
-		if (size==0)
-		{
-			head = temp;
-			mid = temp;
-			size++;
-			return;
-		}
+    void push(int data)
+    {
+        Node *temp = new Node(data);
+        if (size == 0)
+        {
+            head = temp;
+            mid = temp;
+            size++;
+            return;
+        }
 
-		head->next = temp;
-		temp->prev = head;
+        head->next = temp;
+        temp->prev = head;
 
-		//update the pointers
-		head = head->next;
-		if (size%2==1)
-		{
-			mid = mid->next;
-		}
-		size++;
-	}
+        // update the pointers
+        head = head->next;
+        if (size % 2 == 1)
+        {
+            mid = mid->next;
+        }
+        size++;
+    }
 
-	void pop()
-	{
-		if (size!=0)
-		{
-			if (size==1)
-			{
-				head = NULL;
-				mid = NULL;
-			}
-			else
-			{
-				head = head->prev;
-				head->next = NULL;
-				if (size%2==0)
-				{
-					mid = mid->prev;
-				}
-			}
-			size--;
-		}
-	}
+    void pop()
+    {
+        if (size != 0)
+        {
+            if (size == 1)
+            {
+                head = NULL;
+                mid = NULL;
+            }
+            else
+            {
+                head = head->prev;
+                head->next = NULL;
+                if (size % 2 == 0)
+                {
+                    mid = mid->prev;
+                }
+            }
+            size--;
+        }
+    }
 
-	int findMiddle()
-	{
-		if (size==0)
-		{
-			return -1;
-		}
-		return mid->num;
-	}
+    int findMiddle()
+    {
+        if (size == 0)
+        {
+            return -1;
+        }
+        return mid->num;
+    }
 
-	void deleteMiddle()
-	{
-		if (size!=0)
-		{
-			if (size==1)
-			{
-				head = NULL;
-				mid = NULL;
-			}
-			else if (size==2)
-			{
-				head = head->prev;
-				mid = mid->prev;
-				head->next =NULL;
-			}
-			else
-			{
-				mid->next->prev = mid->prev;
-				mid->prev->next = mid->next;
-				if (size%2==0)
-				{
-					mid = mid->prev;
-				}
-				else
-				{
-					mid = mid->next;
-				}
-			}
-			size--;
-		}
-	}
+    void deleteMiddle()
+    {
+        if (size != 0)
+        {
+            if (size == 1)
+            {
+                head = NULL;
+                mid = NULL;
+            }
+            else if (size == 2)
+            {
+                head = head->prev;
+                mid = mid->prev;
+                head->next = NULL;
+            }
+            else
+            {
+                mid->next->prev = mid->prev;
+                mid->prev->next = mid->next;
+                if (size % 2 == 0)
+                {
+                    mid = mid->prev;
+                }
+                else
+                {
+                    mid = mid->next;
+                }
+            }
+            size--;
+        }
+    }
 };
-
 
 int main()
 {
-	myStack st;
-	st.push(11);
-	st.push(22);
-	st.push(33);
-	st.push(44);
-	st.push(55);
-	st.pop();
-	st.pop();
-	st.pop();
-	cout<<st.findMiddle()<<endl;
-	st.deleteMiddle();
-	cout<<st.findMiddle()<<endl;
-	return 0;
+    myStack st;
+    st.push(11);
+    st.push(22);
+    st.push(33);
+    st.push(44);
+    st.push(55);
+    st.pop();
+    st.pop();
+    st.pop();
+    cout << st.findMiddle() << endl;
+    st.deleteMiddle();
+    cout << st.findMiddle() << endl;
+    return 0;
 }
